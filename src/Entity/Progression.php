b43a58c115cf8edb30bc53205aa8aaa2a1394fe4
israa <israa.nasri@esprit.tn>
2026-04-06 19:22:56 +0100

    updated progression

--- src/Entity/Progression.php
+++ src/Entity/Progression.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Entity;
 
 use App\Repository\ProgressionRepository;
@@ -57,31 +58,100 @@ class Progression
     }
 
     #[ORM\PreUpdate]
-    public function onPreUpdate(): void { $this->updatedAt = new \DateTime(); }
+    public function onPreUpdate(): void
+    {
+        $this->updatedAt = new \DateTime();
+    }
 
-    public function getId(): ?int { return $this->id; }
-    public function getUser(): ?User { return $this->user; }
-    public function setUser(?User $v): static { $this->user = $v; return $this; }
-    public function getCours(): ?Cours { return $this->cours; }
-    public function setCours(?Cours $v): static { $this->cours = $v; return $this; }
-    public function getPourcentage(): string { return $this->pourcentage; }
-    public function setPourcentage(string $v): static { $this->pourcentage = $v; return $this; }
-    public function getPointsXp(): int { return $this->pointsXp; }
-    public function setPointsXp(int $v): static { $this->pointsXp = $v; return $this; }
-    public function getNiveau(): int { return $this->niveau; }
-    public function setNiveau(int $v): static { $this->niveau = $v; return $this; }
-    public function getEtat(): string { return $this->etat; }
-    public function setEtat(string $v): static { $this->etat = $v; return $this; }
-    public function getDateDebut(): ?\DateTimeInterface { return $this->dateDebut; }
-    public function getDateObtention(): ?\DateTimeInterface { return $this->dateObtention; }
-    public function setDateObtention(?\DateTimeInterface $v): static { $this->dateObtention = $v; return $this; }
-    public function getUpdatedAt(): ?\DateTimeInterface { return $this->updatedAt; }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+    public function setUser(?User $v): static
+    {
+        $this->user = $v;
+        return $this;
+    }
+    public function getCours(): ?Cours
+    {
+        return $this->cours;
+    }
+    public function setCours(?Cours $v): static
+    {
+        $this->cours = $v;
+        return $this;
+    }
+    public function getPourcentage(): string
+    {
+        return $this->pourcentage;
+    }
+    public function setPourcentage(string $v): static
+    {
+        $this->pourcentage = $v;
+        return $this;
+    }
+    public function getPointsXp(): int
+    {
+        return $this->pointsXp;
+    }
+    public function setPointsXp(int $v): static
+    {
+        $this->pointsXp = $v;
+        return $this;
+    }
+    public function getNiveau(): int
+    {
+        return $this->niveau;
+    }
+    public function setNiveau(int $v): static
+    {
+        $this->niveau = $v;
+        return $this;
+    }
+    public function getEtat(): string
+    {
+        return $this->etat;
+    }
+    public function setEtat(string $v): static
+    {
+        $this->etat = $v;
+        return $this;
+    }
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+    public function setDateDebut(?\DateTimeInterface $d): static
+    {
+        $this->dateDebut = $d;
+        return $this;
+    }
+    public function getDateObtention(): ?\DateTimeInterface
+    {
+        return $this->dateObtention;
+    }
+    public function setDateObtention(?\DateTimeInterface $v): static
+    {
+        $this->dateObtention = $v;
+        return $this;
+    }
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
 
     public function getEtatBadge(): string
     {
         return match ($this->etat) {
-            self::ETAT_NON_COMMENCE => 'secondary', self::ETAT_EN_COURS => 'primary',
-            self::ETAT_COMPLETE => 'success', self::ETAT_CERTIFIE => 'warning', default => 'secondary',
+            self::ETAT_NON_COMMENCE => 'secondary',
+            self::ETAT_EN_COURS => 'primary',
+            self::ETAT_COMPLETE => 'success',
+            self::ETAT_CERTIFIE => 'warning',
+            default => 'secondary',
         };
     }
 }
