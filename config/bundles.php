5e1bfd07e12179f6f60a52d835fa4921b0df3516
Ilyess <ilyessguesmu7@gmail.com>
2026-04-20 22:21:18 +0100

    fix: downgrade endroid/qr-code and phpspreadsheet for PHP 8.2 compatibility

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
-    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
