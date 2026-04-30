f023490b458fc841605211c0d7bbec12e30e21c6
YoussefDmk <167939887+JoeDmk@users.noreply.github.com>
2026-04-30 21:46:05 +0100

    Add installed.php with package version information

---
+++ assets/vendor/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'animejs' => 
+  array (
+    'version' => '4.4.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'three' => 
+  array (
+    'version' => '0.184.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
